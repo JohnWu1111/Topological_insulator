e34a73a5f4990cb09ca9b26a7c45245f6ed999dc
wushuohang <johnwu@sjtu.edu.cn>
2021-08-05 00:12:14 +0800

    update P3.m
    correct the spectrum of the edge state

--- P3.m
+++ P3.m
@@ -1,11 +1,11 @@
-% 2D Chern insulator model, periodical boundary condition for x-direction but
-% finite size for y-direction, square lattice, after Fourier transform for
+% 2D Chern insulator model, periodical boundary condition for y-direction but
+% finite size for x-direction, square lattice, after inversal Fourier transform for
 % x-axis.
 
 tic;
-Lx = 200;
-Ly = 200;
-m = -1;
+Lx = 100;
+Ly = 100;
+m = 1.5;
 sigma_x = [0 1; 1 0];
 sigma_y = [0 -1i; 1i 0];
 sigma_z = [1 0; 0 -1];
@@ -16,22 +16,28 @@ k = -pi+2*pi/Lx:2*pi/Lx:pi;
 % k = 0:2*pi/Lx:2*pi-2*pi/Lx;
 
 for ki = 1:length(k)
-    H1 = zeros(Ly,Ly);
-    H2 = zeros(Ly,Ly);
-    H3 = zeros(Ly,Ly);
-    for i = 1:Ly-1
+    H1 = zeros(Lx,Lx); % for sigma_x
+    H2 = zeros(Lx,Lx); % for sigma_y
+    H3 = zeros(Lx,Lx); % for sigma_z
+    H4 = zeros(Lx,Lx); % for epsilon_k
+    for i = 1:Lx-1
         H1(i,i) = sin(k(ki));
-        H3(i,i) = m + cos(k(ki));
+        H3(i,i) = m - cos(k(ki));
+        H4(i,i) = -2*cos(k(ki));
         
-        H2(i,i+1) = -1i/2;
-        H2(i+1,i) = 1i/2;
-        H3(i,i+1) = 1/2;
-        H3(i+1,i) = 1/2;
+        H2(i,i+1) = 1i/2;
+        H2(i+1,i) = -1i/2;
+        H3(i,i+1) = -1/2;
+        H3(i+1,i) = -1/2;
+        H4(i,i+1) = -1;
+        H4(i+1,i) = -1;
     end
-    H1(Ly,Ly) = sin(k(ki));
-    H3(Ly,Ly) = m + cos(k(ki));
+    H1(Lx,Lx) = sin(k(ki));
+    H3(Lx,Lx) = m - cos(k(ki));
+    H4(Lx,Lx) = -2*cos(k(ki));
     
-    H = kron(H1,sigma_x) + kron(H2,sigma_y) + kron(H3,sigma_z);
+    H = 3*(kron(H1,sigma_x) - kron(H2,sigma_y) + kron(H3,sigma_z)) + kron(H4,eye(2));
+%     H = 3*(kron(H1,sigma_x) - kron(H2,sigma_y) + kron(H3,sigma_z));
     
     e = eig(H);
     x = k(ki).*(zeros(Ly*2,1)+1);
