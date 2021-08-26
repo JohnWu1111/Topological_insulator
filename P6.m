77644023d75663189b5731657edc50d036a33c6b
JohnWu1111 <johnwu@sjtu.edu.cn>
2021-08-26 10:23:36 +0800

    Update P6.m
    Fixed the periodic term

--- P6.m
+++ P6.m
@@ -39,14 +39,14 @@ for j = 1:Ly
         H4(k,k+Lx) = -1;
         H4(k+Lx,k) = -1;
     end
-    %     k = Lx + (j-1)*Ly;
-    %     k0 = (j-1)*Ly;
-    %     H2(k,k0) = 1i/2;
-    %     H2(k0,k) = -1i/2;
-    %     H3(k,k0) = 1/2;
-    %     H3(k0,k) = 1/2;
-    %     H4(k,k0) = 1;
-    %     H4(k0,k) = 1;
+%         k = j + (Lx-1)*Lx;
+%         k0 = j;
+%         H2(k,k0) = 1i/2;
+%         H2(k0,k) = -1i/2;
+%         H3(k,k0) = -1/2;
+%         H3(k0,k) = -1/2;
+%         H4(k,k0) = -1;
+%         H4(k0,k) = -1;
 end
 
 % off-diagonal term, y
@@ -60,14 +60,21 @@ for i = 1:Lx
         H4(k,k+1) = -1;
         H4(k+1,k) = -1;
     end
-%     k = i + (Ly-1)*Ly;
-%     k0 = i;
+    k = Ly + (i-1)*Lx;
+    k0 = 1 + (i-1)*Lx;
 %     H1(k,k0) = -1i/2;
 %     H1(k0,k) = 1i/2;
 %     H3(k,k0) = 1/2;
 %     H3(k0,k) = 1/2;
 %     H4(k,k0) = 1;
 %     H4(k0,k) = 1;
+
+    H1(k,k0) = 1i/2;
+    H1(k0,k) = -1i/2;
+    H3(k,k0) = -1/2;
+    H3(k0,k) = -1/2;
+    H4(k,k0) = -1;
+    H4(k0,k) = -1;
 end
 
 H = 3*(kron(sigma_x,H1) - kron(sigma_y,H2) + kron(sigma_z,H3)) + kron(eye(2),H4);
@@ -107,13 +114,13 @@ for t = 1:L
 end
 
 figure;
-quiver(Jx1,zeros(Lx,Ly))
+quiver(Jx1',zeros(Lx,Ly))
 hold on
-quiver(zeros(Lx,Ly),Jy1)
+quiver(zeros(Lx,Ly),Jy1')
 
 figure;
-quiver(Jx2,zeros(Lx,Ly))
+quiver(Jx2',zeros(Lx,Ly))
 hold on
-quiver(zeros(Lx,Ly),Jy2)
+quiver(zeros(Lx,Ly),Jy2')
 
 toc;
