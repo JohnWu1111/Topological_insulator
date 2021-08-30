eecfd89ab2c48a129c45b5034c9d4a62cb63e845
JohnWu1111 <johnwu@sjtu.edu.cn>
2021-08-30 21:01:44 +0800

    Update P6.m
    Fix the misusing between Lx and Ly

--- P6.m
+++ P6.m
@@ -22,7 +22,7 @@ H4 = zeros(L,L); % for epsilon_k
 % diagonal term
 for i = 1:Lx
     for j = 1:Ly
-        k = j + (i-1)*Lx;
+        k = j + (i-1)*Ly;
         H3(k,k) = m;
 %         H4(k,k) = E*i; %external E-field
     end
@@ -31,15 +31,15 @@ end
 % off-diagonal term, x
 for j = 1:Ly
     for i = 1:Lx-1
-        k = j + (i-1)*Lx;
-        H2(k,k+Lx) = 1i/2;
-        H2(k+Lx,k) = -1i/2;
-        H3(k,k+Lx) = -1/2;
-        H3(k+Lx,k) = -1/2;
-        H4(k,k+Lx) = -1;
-        H4(k+Lx,k) = -1;
+        k = j + (i-1)*Ly;
+        H2(k,k+Ly) = 1i/2;
+        H2(k+Ly,k) = -1i/2;
+        H3(k,k+Ly) = -1/2;
+        H3(k+Ly,k) = -1/2;
+        H4(k,k+Ly) = -1;
+        H4(k+Ly,k) = -1;
     end
-%         k = j + (Lx-1)*Lx;
+%         k = j + (Lx-1)*Ly;
 %         k0 = j;
 %         H2(k,k0) = 1i/2;
 %         H2(k0,k) = -1i/2;
@@ -52,7 +52,7 @@ end
 % off-diagonal term, y
 for i = 1:Lx
     for j = 1:Ly-1
-        k = j + (i-1)*Lx;
+        k = j + (i-1)*Ly;
         H1(k,k+1) = 1i/2;
         H1(k+1,k) = -1i/2;
         H3(k,k+1) = -1/2;
@@ -60,8 +60,8 @@ for i = 1:Lx
         H4(k,k+1) = -1;
         H4(k+1,k) = -1;
     end
-    k = Ly + (i-1)*Lx;
-    k0 = 1 + (i-1)*Lx;
+    k = Ly + (i-1)*Ly;
+    k0 = 1 + (i-1)*Ly;
 %     H1(k,k0) = -1i/2;
 %     H1(k0,k) = 1i/2;
 %     H3(k,k0) = 1/2;
@@ -69,15 +69,16 @@ for i = 1:Lx
 %     H4(k,k0) = 1;
 %     H4(k0,k) = 1;
 
-    H1(k,k0) = 1i/2;
-    H1(k0,k) = -1i/2;
-    H3(k,k0) = -1/2;
-    H3(k0,k) = -1/2;
-    H4(k,k0) = -1;
-    H4(k0,k) = -1;
+%     H1(k,k0) = 1i/2;
+%     H1(k0,k) = -1i/2;
+%     H3(k,k0) = -1/2;
+%     H3(k0,k) = -1/2;
+%     H4(k,k0) = -1;
+%     H4(k0,k) = -1;
 end
 
 H = 3*(kron(sigma_x,H1) - kron(sigma_y,H2) + kron(sigma_z,H3)) + kron(eye(2),H4);
+% H = 3*( kron(sigma_y,H2) + kron(sigma_z,H3)) + kron(eye(2),H4);
 
 [phi,e] = eig(H);
 
@@ -90,10 +91,10 @@ Jy2 = zeros(Lx,Ly);
 for t = 1:L
     for i = 1:Lx
         for j = 1:Ly
-            a = (i-1)*Lx+j;
-            b = Lx*Ly+(i-1)*Lx+j;
-            phi_xy1(i,j) = phi((i-1)*Lx+j,t);
-            phi_xy2(i,j) = phi(Lx*Ly+(i-1)*Lx+j,t);
+            a = (i-1)*Ly+j;
+            b = L+(i-1)*Ly+j;
+            phi_xy1(i,j) = phi((i-1)*Ly+j,t);
+            phi_xy2(i,j) = phi(L+(i-1)*Ly+j,t);
         end
     end
     
@@ -118,15 +119,22 @@ end
 Jx = Jx1 + Jx2;
 Jy = Jy1 + Jy2;
 
-figure;
-quiver(30*Jx1',zeros(Lx,Ly),0)
-hold on
-quiver(zeros(Lx,Ly),30*Jy1',0)
+Jy1_left = sum(Jy1(1,:))
+Jy1_right = sum(Jy1(end,:))
+Jy2_left = sum(Jy2(1,:))
+Jy2_right = sum(Jy2(end,:))
+Jy_left = sum(Jy(1,:))
+Jy_right = sum(Jy(end,:))
 
 figure;
-quiver(30*Jx2',zeros(Lx,Ly),0)
+quiver(30*Jx1',zeros(Ly,Lx),0)
 hold on
-quiver(zeros(Lx,Ly),30*Jy2',0)
+quiver(zeros(Ly,Lx),30*Jy1',0)
+
+figure;
+quiver(30*Jx2',zeros(Ly,Lx),0)
+hold on
+quiver(zeros(Ly,Lx),30*Jy2',0)
 
 % figure;
 % quiver(Jx1'+Jx2',zeros(Lx,Ly))
